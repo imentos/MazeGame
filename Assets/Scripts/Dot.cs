35d44d84bdf6f3759bd0269056bec93d0e95adf9
Ray Kuo <ray.kuo@sap.com>
2018-12-17 15:59:38 -0800

    First commit

---
+++ Assets/Scripts/Dot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour {
+    public float speed = 10f;
+    public Vector2 moving = new Vector2();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var forceX = 0f;
+        //var forceY = 0f;
+        
+        //if (Input.GetKey("right"))
+        //{
+        //    moving.x = 1;
+        //}
+        //else if (Input.GetKey("left"))
+        //{
+        //    moving.x = -1;
+        //}
+
+        //if (Input.GetKey("up"))
+        //{
+        //    moving.y = 1;
+        //}
+        //else if (Input.GetKey("down"))
+        //{
+        //    moving.y = -1;
+        //}
+
+        //forceX = speed * moving.x;
+        //forceY = speed * moving.y;
+
+        //GetComponent<Rigidbody2D>().AddForce(new Vector2(forceX, forceY));
+
+        ////GetComponent<Rigidbody2D>().transform.position = 
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        GetComponent<Rigidbody2D>().AddForce(movement * speed);
+	}
+}
---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetMouseButtonDown(0)) {
+        //    animator.SetTrigger("out");
+        //}
+
+	}
+
+    public void onFadeComplete() {
+        SceneManager.LoadScene("Level1");
+    }
+}
